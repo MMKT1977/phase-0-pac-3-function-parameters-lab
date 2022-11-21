d5b2aafdf418d9153040210facc953be971c2ae5
Kennedy Moranga <kennedy.moranga@student.moringaschool.com>
2022-11-21 03:21:13 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,8 @@
+function logTwoValues(value1=1, value2=7) {
+    console.log(`The two values are ${value1} and ${value2}.`);
+  }
+logTwoValues();
+function introductionWithLanguageOptional(firstName="Kennedy", language="Javascript") {
+    console.log(`Hello, ${firstName}!+ ${language}`);
+  }
+introductionWithLanguageOptional();
